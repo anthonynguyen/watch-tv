9621f9cb825b97f735b2593b876e4c16ddcadc08
Anthony Nguyen <anknguyen@gmail.com>
2014-05-28 17:55:21 -0400

    Switched from parsing HTML to using regexes in watchseries backend

--- backends/test.py
+++ backends/test.py
@@ -3,7 +3,6 @@ Backend file for interfacing with online TV episode aggregators
 This test backend is currently set up to use watchseries
 """
 
-import html.parser
 import re
 import urllib.request
 
@@ -11,53 +10,26 @@ id = "watchseries"
 
 SEARCH_URL = "http://www.watchseries.to/search/{}"
 SHOW_URL = "http://www.watchseries.to/serie/{}" # e.g. how_i_met_your_mother
+EPISODE_URL = "http://www.watchseries.to/episode/{}.html" # e.g. how_i_met_your_mother_s1_e1
+VIDEO_AD_URL = "http://www.watchseries.to/open/cale/{}.html" # e.g. 1289651
 
-class searchParser(html.parser.HTMLParser):
-	position = 0 # -1 -> nowhere, 1 -> in table, 2 -> in td, 3 -> in Link
-	interest = 0
-	results = {}
-	def handle_starttag(self, tag, attrs):
-		if tag == "table":
-			self.position = 1
-		elif tag == "td" and self.position == 1:
-			for attr in attrs:
-				if attr[0] == "style" and attr[1] == "padding-left: 10px;":
-					self.position = 2
-		elif tag == "a" and self.position == 2:
-			# attrs shoudl be in the following form:
-			# [('href', '/serie/how_i_met_your_mother'), ('title', 'How I Met Your Mother')]
-			self.results[attrs[0][1][7:]] = attrs[1][1]
-			self.position = 3
-
-	def handle_endtag(self, tag):
-		if tag == "a" and self.position == 3:
-			self.position = 2
-		elif tag == "td" and self.position == 2:
-			self.position = 1
-		elif tag == "table":
-			self.position = 0
-
-	def handle_data(self, data):
-		if self.position > 2:
-			print(self.position)
-			print(data)
-		if self.position == 4:
-			self.results.append(data)
-
-	def getResults(self):
-		return self.results
-
+SEARCH_RE = re.compile(r"\<a href=\"/serie/(.+?)\" title=\"(.+?)\"\>")
 
 def search(query):
+	results = {}
+
 	req = urllib.request.urlopen(SEARCH_URL.format(query))
 	data = req.read().decode("utf-8")
 
-	p = searchParser()
-	p.feed(data)
-	return p.getResults()
+	for m in SEARCH_RE.finditer(data):
+		results[m.group(1)] = m.group(2)
 
-def getShow():
+	return results
+
+
+def getShow(showID): # showID -> e.g. how_i_met_your_mother
 	# Ideally, getShow would return a list of lists of episode links
+
 	pass
 
 def getEpisode():
