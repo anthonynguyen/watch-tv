2fc6d9a8b9a513c2c3993ae5503db8e8a5575fd9
Anthony Nguyen <anknguyen@gmail.com>
2014-06-03 21:59:44 -0400

    Added video host: movpod

---
+++ video_hosts/movpod.py
@@ -0,0 +1,39 @@
+"""
+Backend file for interfacing with video hosts
+This backend is for movpod.in
+"""
+
+import re
+import urllib.parse
+import urllib.request
+
+id = "movpod"
+domain = "movpod.in"
+
+FNAME_RE = re.compile(r"file: \"(.+?/video.flv)\",")
+
+# Send POST to http://movpod.in/videoID
+#    + op -> download1
+#    + id -> videoID
+#    + method_free -> Free Download
+
+def getVid(videoID): # e.g. 59k4uhwsye0b
+	params = {
+		"op": "download1",
+		"id": videoID,
+		"method_free": "Free Download"
+	}
+
+	p = urllib.parse.urlencode(params)
+	p = p.encode("ascii")
+
+	req = urllib.request.urlopen("http://movpod.in/{}".format(videoID), data = p)
+	data = req.read().decode("utf-8")
+
+	match = FNAME_RE.search(data)
+	if match == None:
+		return "Could not pull video from link"
+
+	fname = match.group(1)
+
+	return fname
